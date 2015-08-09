afd5f9e8146dda44be51339cee9d4c018128f585
briahnna123 <briahnna123@users.noreply.github.com>
2015-08-08 21:08:36 -0700

    First changes

---
+++ plot_2.R
@@ -0,0 +1,22 @@
+# Plot_2 code
+
+# Introduction
+# This assignment uses data from the UC Irvine Machine Learning Repository, a 
+# popular repository for machine learning datasets. In particular, we will be 
+# using the “Individual household electric power consumption Data Set” which I 
+# have made available on the course web site:
+
+# Making Plots
+# For each plot you should Construct the plot and save it to a PNG file with a 
+# width of 480 pixels and a height of 480 pixels. Name each of the plot files as 
+# plot1.png, plot2.png, etc.
+
+
+
+
+
+
+
+
+
+
