ea7fbdadbc8e56d94cff46a43c543e3676a0085c
briahnna123 <briahnna123@users.noreply.github.com>
2015-08-08 21:52:23 -0700

    Next changes

--- plot_1.R
+++ plot_1.R
@@ -11,6 +11,32 @@
 # width of 480 pixels and a height of 480 pixels. Name each of the plot files as 
 # plot1.png, plot2.png, etc.
 
+# Step 1: Bring in the data to manipulate
+setwd("~/Desktop") # Set WD
+files <- file("household_power_consumption.txt") # Bring in file
+power_data <- read.table(files, header=T, sep=";") # Read data into table
+
+
+# Step 2: We need to "clean" the data; making tidy data
+# Convert the Time format to: Day, Month, Year
+power_data$Date <- as.Date(power_data$Date, format="%d/%m/%Y")
+
+
+# Step 3: Subset the data to the dates we will only me manipulating 
+# Here we are saying we want all Columns were data is between sepcific dates
+data<- power_data[power_data$Date >= as.Date("2007-02-01") & power_data$Date<=as.Date("2007-02-02"),]
+
+
+# Step 4: Make Columns numeric
+data$Global_active_power<- as.numeric(as.character(data$Global_active_power))
+
+# Step 5: Make Histogram
+png(filename= "plot1.png", width=480, height=480, units="px")
+hist(data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+dev.off() # Show the graph in the WD
+
+
+
 
 
 
